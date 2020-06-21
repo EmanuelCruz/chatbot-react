bf68ee9398b44a97626499f518ab4afffbc12a94
EmanuelCruz <emanuelcruz091@gmail.com>
2020-06-21 00:05:25 -0300

    Agrego estilos para el home y efectos

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,11 +1,22 @@
 import React from "react";
 import Bot from "./components/Bot/Bot";
+import "./Home.css";
+import Pulse from "react-reveal/Pulse";
 
 const Home = ({ history }) => {
   return (
-    <div>
-      <Bot cat history={history} />
-      <label>¡¡BUENOS DIAS!!</label>
+    <div className="home-bot-container">
+      <div className="home-bot-content">
+        <Bot cat history={history} />
+        <div className="home-bot-greeting">
+          <Pulse>
+            <h1 className="titulo">¡¡Buenos Días!!</h1>
+          </Pulse>
+          <Pulse>
+            <label className="subtitulo">Presióname para saber más.</label>
+          </Pulse>
+        </div>
+      </div>
     </div>
   );
 };
