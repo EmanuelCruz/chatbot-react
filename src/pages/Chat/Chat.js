7f18c579fd198d6ab9138ef832697576c2f8d228
EmanuelCruz <emanuelcruz091@gmail.com>
2020-06-26 19:00:47 -0300

    Maquetacion de la estructura inicial del chat

--- src/pages/Chat/Chat.js
+++ src/pages/Chat/Chat.js
@@ -2,7 +2,15 @@ import React from "react";
 import "./Chat.css";
 
 const Chat = () => {
-  return <div>Soy un chat</div>;
+  return (
+    <div className="chatbot-chat-container">
+      <div className="chatbot-chat-content">
+        <div className="chatbot-chat">
+          <div className="chatbot-chat-container-body">Cuerpo y mensajes</div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Chat;
