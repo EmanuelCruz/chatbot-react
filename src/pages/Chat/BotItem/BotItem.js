fb5bab22fa031e7f00153dae35fa441235a5a494
EmanuelCruz <emanuelcruz091@gmail.com>
2020-07-04 01:56:08 -0300

    Eliminacion de efectos de los mensajes

--- src/pages/Chat/BotItem/BotItem.js
+++ src/pages/Chat/BotItem/BotItem.js
@@ -1,20 +1,15 @@
 import React from "react";
 import "./BotItem.css";
 import botAvatar from "../../../assets/imagenes/sunny.png";
-import Fade from "react-reveal/Fade";
-import Zoom from "react-reveal/Zoom";
 
 const BotItem = ({ text }) => {
   return (
     <div className="bot-item-container">
-      <Zoom>
-        <img src={botAvatar} alt="bot-avatar"></img>
-      </Zoom>
+      <img src={botAvatar} alt="bot-avatar"></img>
+
       <div className="bot-item-messages">
         {text.map((t, index) => (
-          <Fade left>
-            <label key={index}>{t}</label>
-          </Fade>
+          <label key={index}>{t}</label>
         ))}
       </div>
     </div>
--- src/pages/Chat/UserItem/UserItem.js
+++ src/pages/Chat/UserItem/UserItem.js
@@ -1,20 +1,14 @@
 import React from "react";
 import "./UserItem.css";
 import user from "../../../assets/imagenes/user.png";
-import Fade from "react-reveal/Fade";
-import Zoom from "react-reveal/Zoom";
 
 const UserItem = ({ text }) => {
   return (
     <div className="user-item-container">
-      <Fade right>
-        <div className="user-item-messages">
-          <label>{text}</label>
-        </div>
-      </Fade>
-      <Zoom>
-        <img src={user} alt="woman-user-avatar" />
-      </Zoom>
+      <div className="user-item-messages">
+        <label>{text}</label>
+      </div>
+      <img src={user} alt="woman-user-avatar" />
     </div>
   );
 };
