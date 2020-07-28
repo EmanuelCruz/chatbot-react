33c066e2b5d6af40fc49b087804d3a93ccfbcf16
EmanuelCruz <emanuelcruz091@gmail.com>
2020-07-28 19:10:56 -0300

    Mensajes del bot

--- src/data/Actions.js
+++ src/data/Actions.js
@@ -3,7 +3,7 @@ export let doing = [
     msg: ["Estaba preparandome para salir."],
   },
   {
-    msg: ["Estoy de mal humor.", "Si salis mejor lleva paraguas..."],
+    msg: ["Estoy de mal humor.", "Si salis, mejor lleva paraguas..."],
   },
   {
     msg: ["Me quede dormido.", "Me acoste tarde"],
@@ -11,16 +11,25 @@ export let doing = [
   {
     msg: ["Me levante con mucha energia.", "Prepara el aire acondicionado"],
   },
+  {
+    msg: ["Solo voy a decir esto", "Lindo día para estar en la playa"],
+  },
 ];
 
 export let aboutMe = [
   {
-    msg: ["Me gusta ser el centro del universo"],
+    msg: ["Me gusta ser el centro del Universo."],
   },
   {
-    msg: ["Me gusta los eclipses.", "Mas si son solares"],
+    msg: ["Me gusta los eclipses.", "Más si son solares."],
   },
   {
-    msg: ["No le digas a nadie pero...", "la luna no me cae tan bien"],
+    msg: ["No tengo mucho para decirte.", "Preguntame más tarde."],
+  },
+  {
+    msg: [
+      "El verano es mi estación favorita",
+      "Aunque la primavera no se queda atrás.",
+    ],
   },
 ];
